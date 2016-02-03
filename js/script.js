81153d85648a99295b8f213e59a0f69a6bf9891c
am5801 <am5801@nyu.edu>
2016-02-03 16:25:09 -0500

    Assignment 2

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('div').on('click', function() {
+  $(this).toggleClass('show-description');
+});
+
+$('#black').click(function(){
+  $('header').css('background-image',"url('http://mechanical-enterprises.com/wp-content/uploads/2013/05/chalkboard.jpg')");
+});
+
+$('#red').click(function(){
+  $('header').css('background-image',"url('http://encorestudio.biz/wp-content/uploads/2013/05/Red-Encore-Chalkboard-Background.png')");
+});
+
+$('#blue').click(function(){
+  $('header').css('background-image',"url('http://i.istockimg.com/file_thumbview_approve/18288140/3/stock-photo-18288140-chalkboard-dark-blue-blackboard-texture-background.jpg')");
+});
+
+$('#green').click(function(){
+  $('header').css('background-image',"url('https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT69Rncy_12ZfuCjxNxGxIXdHotOiO2NvCurLDot5RcOoICtzPJzg')");
+});
+
+$('#purple').click(function(){
+  $('header').css('background-image',"url('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRRu9REAKbD1CYt_cENp_CfNxW3TSoDthlTO1c33II-El0g5Pc9')");
+});
